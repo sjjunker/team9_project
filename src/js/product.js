28bfe97b92c3cefff2f5070b37af72df6ec0d13f
Jgohnert <goh22001@byui.edu>
2024-09-26 15:52:10 -0600

    fixed the briken cart list

--- src/js/product.js
+++ src/js/product.js
@@ -2,7 +2,11 @@ import { setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cartList = JSON.parse(localStorage.getItem("so-cart")) || [];
+
+  cartList.push(product);
+  
+  setLocalStorage("so-cart", cartList);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
@@ -11,6 +15,6 @@ async function addToCartHandler(e) {
 }
 
 // add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const cartButton = document.getElementById("addToCart")
+
+cartButton.addEventListener("click", addToCartHandler);
