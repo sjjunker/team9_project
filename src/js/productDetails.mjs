b51d68418c49fa18640bdb5c48ebe3a0e147220d
Sandi Junker <sandi.junker@outlook.com>
2024-10-03 17:42:47 -0500

    Delete addToCartHandler, delete local parameter within addProductToCart

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -6,10 +6,10 @@ var product = {};
 export default async function productDetails(productId) {
   product = await findProductById(productId)
   renderProductDetails();
-  document.getElementById("addToCart").addEventListener("click", addToCartHandler);
+  document.getElementById("addToCart").addEventListener("click", addProductToCart);
 }
 
-function addProductToCart(product) {
+function addProductToCart() {
   // Retrieve the existing cart from localStorage, ensure it's an array or default to an empty array
   let cart = JSON.parse(localStorage.getItem("so-cart"));
 
@@ -23,10 +23,6 @@ function addProductToCart(product) {
 
   // Save the updated cart back to localStorage
   setLocalStorage("so-cart", cart);
-}
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
 }
 
 function renderProductDetails() {
