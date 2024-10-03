ce9c55ccd1cbdf3fb0e0ab1c9fde6ed4843b7fbd
Sandi Junker <sandi.junker@outlook.com>
2024-10-03 12:42:55 -0500

    Add script to product pages index.html

--- src/js/shake.js
+++ src/js/shake.js
@@ -3,8 +3,5 @@ let cartIcon = document.querySelector(".cart");
 
 //Eventlistener
 addToCartButton.addEventListener("click", () => {
-  cartIcon.classList.add("apply-shake");
+    cartIcon.classList.add('apply-shake');
 });
-
-//TODO: Add script to product_pages/index.html when completed with team
-//<script src="../js/shake.js" defer></script>
