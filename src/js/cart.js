b526992de0f1aef9e7292c9e99f2b48d730eda26
Sandi Junker <sandi.junker@outlook.com>
2024-10-11 10:46:39 -0500

    Add delete item from cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,13 +1,16 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
+//Show cart items to screen
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 
   displayTotal(cartItems);
+  deleteItem();
 }
 
+//Create the HTML list of items
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
@@ -22,30 +25,52 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <button type="button" class="delete_button" id="button${item.Id}"><span data-id=${item.Id}>X</span></button>
 </li>`;
 
   return newItem;
 }
 
+//Attach eventListener to the delete-button
+function deleteItem() {
+  //Grab localstorage
+  const cartItems = getLocalStorage("so-cart");
+
+  //Add listener to each button
+  cartItems.forEach((item, index) => {
+    const deleteButton = document.getElementById(`button${item.Id}`);
+
+    deleteButton.addEventListener("click", () => {
+      const cartWithoutItem = cartItems.filter((item) => item != cartItems[index]);
+      setLocalStorage("so-cart", cartWithoutItem);
+      renderCartContents();
+    })
+  })
+}
+
+
+//Cart total
 const cartFooter = document.querySelector(".cart-footer");
 const cartTotal = document.querySelector(".cart-total");
 
+//Compute the cart total
 function totalCost(cartItems) {
-  if (!cartItems.length) {
+  let total = 0;
+
+  if (!cartItems.length > 0) {
     cartFooter.style.display = "none";
   } else {
     cartFooter.style.display = "block";
 
-    let total = 0;
-
     cartItems.forEach((item) => {
       total += item.FinalPrice;
     });
-
-    return total;
   }
+
+  return total;
 }
 
+//Display cart total to screen
 function displayTotal(cartItems) {
   const sum = totalCost(cartItems);
 
