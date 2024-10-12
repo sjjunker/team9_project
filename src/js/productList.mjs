9c3f8c7ed7ff02a83aa4964041679344e0afba75
SpaceSettler <j.devon.richardson@gmail.com>
2024-10-11 17:37:11 -0700

    The home page now shows a percent discount.

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -12,6 +12,8 @@ export default async function productList(selector, category) {
 }
 
 function productCardTemplate(product) {
+  let discount = Math.round(100 - (product.FinalPrice / product.SuggestedRetailPrice * 100));
+
     return `<li class="product-card">
     <a href="product_pages/index.html?product=${product.Id}">
     <img
@@ -20,6 +22,6 @@ function productCardTemplate(product) {
     />
     <h3 class="card__brand">${product.Brand.Name}</h3>
     <h2 class="card__name">${product.NameWithoutBrand}</h2>
-    <p class="product-card__price">$${product.FinalPrice}</p></a>
+    <p class="product-card__price">$${product.FinalPrice} ~${discount}% off</p></a>
   </li>`;
 }
