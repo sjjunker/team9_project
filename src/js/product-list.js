9dca6db852b31b303b9f1ff1a9439b18d634ec18
Sandi Junker <sandi.junker@outlook.com>
2024-10-29 18:41:00 -0500

    Add breadcrumbs to each page.

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -1,5 +1,7 @@
 import { productList } from "./productList.mjs";
 import { loadHeaderFooter, getParam } from "./utils.mjs";
+import { getData } from "./productData.mjs";
+import setBreadcrumbs from "./breadcrumbs";
 
 //Load page depending on whether it is called via link
 let category = getParam("category");
@@ -11,3 +13,9 @@ if (category != null) {
 }
 
 loadHeaderFooter();
+
+//Set breadcrumbs
+let products = await getData(category);
+let capitalizedCategory = category.charAt(0).toUpperCase() + category.substring(1);
+let productQuantity = products.length;
+setBreadcrumbs(capitalizedCategory, productQuantity);
