78146befbd9963e21f55e9fa416e5bde0ad37205
Sandi Junker <sandi.junker@outlook.com>
2024-11-15 14:07:53 -0600

    little debugging and styling

--- src/js/signup.mjs
+++ src/js/signup.mjs
@@ -1,13 +1,14 @@
-import { loadHeaderFooter, alertMessage, removeAllAlerts} from "./utils.mjs";
+import { loadHeaderFooter, alertMessage, removeAllAlerts } from "./utils.mjs";
 import { signUp } from "./externalServices.mjs";
 
 loadHeaderFooter();
 document.querySelector('#signup').addEventListener('click', async (e) => {
   e.preventDefault();
-  let name = document.querySelector('#name');
-  let address = document.querySelector('#address');
-  let email = document.querySelector('#email');
-  let password = document.querySelector('#password');
+  let name = document.querySelector('#name').value;
+  let address = document.querySelector('#address').value;
+  let email = document.querySelector('#email').value;
+  let password = document.querySelector('#password').value;
+
   try {
     const res = await signUp({ name, address, email, password });
     console.log(res);
